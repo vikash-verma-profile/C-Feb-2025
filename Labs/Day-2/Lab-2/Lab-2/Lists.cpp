b7638fa348cc67bc3e7d77ac111b1d20ac1ba105
vikash-verma-profile <er.vikashverma@yahoo.com>
2025-02-18 15:23:19 +0530

    added STLs

---
+++ Labs/Day-2/Lab-2/Lab-2/Lists.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+	list<int> samplelist{ 12,24,23,12 };
+	/*for (auto i : samplelist)
+	{
+		cout << i << ' ';
+	}*/
+
+	list<int> list2;
+
+	for (int i = 0; i < 10; i++)
+	{
+		list2.push_back(12);
+		list2.push_front(10);
+	}
+	list2.pop_back();
+	list2.pop_front();
+	list2.reverse();
+	list2.sort();
+	//list2.clear();
+	list2.remove(10);
+	if (list2.empty()) {
+		cout << "list is  empty";
+	}
+	else {
+		cout << "list is  not empty";
+	}
+	for (auto i : list2)
+	{
+		cout << i << ' ';
+	}
+	return 0;
+}
--- Labs/Day-2/Lab-2/Lab-2/Source.cpp
+++ Labs/Day-2/Lab-2/Lab-2/Source.cpp
@@ -8,7 +8,7 @@ void printV(vector<int> &v) {
 		cout << v[i] << " ";
 	}
 }
-int main() {
+int main1() {
 	vector<int> v1;
 	vector<int> v2 = { 1,2,3,4,5 };
 
